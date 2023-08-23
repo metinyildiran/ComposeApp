f65bba5ed472a309eac49ee8e91e5d0e70dd465a
metinyildiran <metin.1464@gmail.com>
2023-08-23 16:08:04 +0300

    Added something

--- app/src/main/java/com/example/composeapp/ui/MainScreen.kt
+++ app/src/main/java/com/example/composeapp/ui/MainScreen.kt
@@ -2,14 +2,26 @@ package com.example.composeapp.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(viewModel: MainViewModel = hiltViewModel()) {
+    val emailValue = rememberSaveable { mutableStateOf("") }
+    var passwordValue by rememberSaveable { mutableStateOf(viewModel.clickCount.value.toString()) }
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
@@ -21,5 +33,13 @@ fun MainScreen(viewModel: MainViewModel = hiltViewModel()) {
         }) {
             Text(text = "Click Me")
         }
+
+        TextField(modifier = Modifier.fillMaxWidth(), value = emailValue.value, onValueChange = {
+            emailValue.value = it
+        }, maxLines = 1)
+
+        TextField(value = passwordValue, onValueChange = {
+            passwordValue = it
+        })
     }
 }
